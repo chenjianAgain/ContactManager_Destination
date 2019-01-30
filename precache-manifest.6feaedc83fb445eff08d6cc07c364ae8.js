cc68da906c401056a1abc8f62d72a5bfee6b6f15
Joseph <joseph.siyi@gmail.com>
2019-01-30 15:34:09 +0000

    first build cp here

---
+++ precache-manifest.6feaedc83fb445eff08d6cc07c364ae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14bac68aaf0491b94f9b",
+    "url": "/static/css/main.b2c19ecd.chunk.css"
+  },
+  {
+    "revision": "14bac68aaf0491b94f9b",
+    "url": "/static/js/main.14bac68a.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "21be7cf395433a2a1c3fd5d8420a804b",
+    "url": "/index.html"
+  }
+];
