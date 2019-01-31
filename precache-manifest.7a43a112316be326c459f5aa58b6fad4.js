29c8b20e3f50342c1f1b19c4ff0d32a7693493bb
Joseph <joseph.siyi@gmail.com>
2019-01-31 00:07:24 +0000

    change sth to fit Github Pages

---
+++ precache-manifest.7a43a112316be326c459f5aa58b6fad4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04b9d1cb819b976d05ca",
+    "url": "/ContactManager_Destination/static/css/main.b2c19ecd.chunk.css"
+  },
+  {
+    "revision": "04b9d1cb819b976d05ca",
+    "url": "/ContactManager_Destination/static/js/main.04b9d1cb.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ContactManager_Destination/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7a2d4c0ccd5904a9e5f0",
+    "url": "/ContactManager_Destination/static/js/runtime~main.7a2d4c0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ContactManager_Destination/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2e80278e94d8f2352294859c8d676bd7",
+    "url": "/ContactManager_Destination/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6feaedc83fb445eff08d6cc07c364ae8.js"
+  "/ContactManager_Destination/precache-manifest.7a43a112316be326c459f5aa58b6fad4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ContactManager_Destination/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
